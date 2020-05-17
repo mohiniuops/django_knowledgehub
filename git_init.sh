0892a888e2958c12076685663f27ef0d5e040ff5
mohinid <mohini4git@gmail.com>
2020-05-17 04:48:43 +0000

    Commited Django Code

--- git_init.sh
+++ git_init.sh
@@ -4,7 +4,7 @@ git config --global user.email "mohini4git@gmail.com"
 git config --global user.name "mohinid"
 git add git_init.sh
 git commit -m "Commited Django Code"
-git remote add origin https://github.com/mohiniuops/django_knowledgehub.git
+#git remote add origin https://github.com/mohiniuops/django_knowledgehub.git
 git push https://'mohiniuops':'Urminalu@9'@'github.com/mohiniuops/django_knowledgehub.git'
 #git remote add origin https://github.com/mohiniuops/django_knowledgehub.git
 #git push -u origin master
