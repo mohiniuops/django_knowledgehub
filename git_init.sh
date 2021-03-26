807ad26a2239e464218a4b20075da12b6b8a044f
mohiniuops <63095966+mohiniuops@users.noreply.github.com>
2021-03-26 12:13:31 +0530

    Update git_init.sh

--- git_init.sh
+++ git_init.sh
@@ -6,6 +6,6 @@ git add git_init.sh
 git add django_app
 git commit -m "Commited Django Code"
 #git remote add origin https://github.com/mohiniuops/django_knowledgehub.git
-git push https://'mohiniuops':'Urminalu%409'@'github.com/mohiniuops/django_knowledgehub.git'
+git push https://'mohiniuops':'$PASS'@'github.com/mohiniuops/django_knowledgehub.git'
 #git remote add origin https://github.com/mohiniuops/django_knowledgehub.git
 #git push -u origin master
